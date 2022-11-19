604eb79e5b178654dd631b89d6bf0eb1c0fded97
AWard59 <iamsancho5990@hotmail.com>
2022-11-18 21:34:06 -0800

    feat: Home component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+		<>
+			<br />
+			<div className='container'>
+				<h1>Welcome to Plenty of CRUD</h1>
+				<br />
+				<h5>
+					If you are new, sign up now to be able to view other members!
+				</h5>
+			</div>
+		</>
+	)
+}

1676ff29f5e2f9dd1c70ec11d2fba3226980c8c8
AWard59 <iamsancho5990@hotmail.com>
2022-11-18 22:04:41 -0800

    feat: updated App.js with Auth and Home components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
-import logo from './assets/logo.svg';
+import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
+
+import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
+import Header from './components/Header'
+import Home from './components/Home'
+
+import SignUp from './components/auth/SignUp'
+import SignIn from './components/auth/SignIn'
+import SignOut from './components/auth/SignOut'
+import ChangePassword from './components/auth/ChangePassword'
 
 const App = () => {
+  const [user, setUser] = useState(null)
+  const [msgAlerts, setMsgAlerts] = useState([])
+
+  const clearUser = () => setUser(null)
+
+  const msgAlert = ({ heading, message, variant }) => {
+    const id = uuid()
+    setMsgAlerts(msgAlerts => ([...msgAlerts, { heading, message, variant, id }]))
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <Header user={user} />
+      {msgAlerts.map((msgAlert) => (
+        <AutoDismissAlert
+          key={msgAlert.id}
+          heading={msgAlert.heading}
+          variant={msgAlert.variant}
+          message={msgAlert.message}
+          id={msgAlert.id}
+        />
+      ))}
+      <main className='container'>
+        <Routes>
+          {/* Auth Routes */}
+          <Route
+            path='/sign-up/'
+            element={<SignUp msgAlert={msgAlert} setUser={setUser} />}
+          />
+          <Route
+            path='/sign-in/'
+            element={<SignIn msgAlert={msgAlert} setUser={setUser} />}
+          />
+          <Route
+            path='/sign-out/'
+            element={
+              <SignOut
+                msgAlert={msgAlert}
+                user={user}
+                clearUser={clearUser}
+              />
+            }
+          />
+          <Route
+            path='/change-password/'
+            element={<ChangePassword msgAlert={msgAlert} user={user} />}
+          />
+          {/* Generic Routes */}
+          <Route path='/' element={<Home user={user} />} />
+        </Routes>
+      </main>
+    </>
   )
 }
 

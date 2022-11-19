94add70e57973b2f4f15fa4aaccc5f9e7508edd6
AWard59 <iamsancho5990@hotmail.com>
2022-11-18 21:27:34 -0800

    feat: api auth connections

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { BrowserRouter } from 'react-router-dom'
+
+const appJsx = (
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<App />
+	</BrowserRouter>
+)
+
+ReactDOM.render(appJsx, document.getElementById('root'))

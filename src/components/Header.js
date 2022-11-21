1478a9aeee21bbee6f60b284e31146d214cea6d2
AWard59 <iamsancho5990@hotmail.com>
2022-11-20 22:08:43 -0800

    convert css files to scss, import bootstrap, color nav & add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import { Link, NavLink } from 'react-router-dom'
 
-// import '../assets/navStyle.css'
+import '../assets/navStyle.scss'
 
 const unauthenticatedOptions = (
   <>
@@ -15,20 +15,21 @@ const unauthenticatedOptions = (
 )
 
 const Header = ({ user }) => (
-	<Navbar bg='primary' variant='dark' expand='md'>
+	<Navbar className='navBar' variant='light' expand='md'>
 		<Container>
 			<Navbar.Brand>
-				{/* <Link to='/courses/' style={{ color: '#FFF', textDecoration: 'none' }}>
-					<img className='navLogo' src='https://i.imgur.com/E9e7s2r.png'></img>
-				</Link> */}
+				<Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>
+					<img
+						className='navLogo'
+						alt=''
+						src='https://i.imgur.com/n7FH6re.png'></img>
+				</Link>
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls='basic-navbar-nav' />
 			<Navbar.Collapse id='basic-navbar-nav'>
 				<Nav className='ms-auto'>
 					{user && (
-						<span className='navbar-text me-2'>
-							Welcome, {user.email}
-						</span>
+						<span className='navbar-text me-2'>Welcome, {user.email}</span>
 					)}
 					{unauthenticatedOptions}
 				</Nav>
